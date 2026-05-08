f8fdfbb04e8b3fe31b2db70a5c5d86512896ba50
murugan anna <vishalwin@gmail.com>
2026-05-08 11:21:27 +0530

    cross browser testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,30 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.edge.service import Service
 
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-
-
+def pytest_addoption(parser):
+   parser.addoption(
+      "--browser",
+      action="store",
+      default ="chrome"
+   )
 @pytest.fixture(scope="function")
-def driver():
-    service =Service(
-       ChromeDriverManager().install()
-    )
-    
-    driver= webdriver.Chrome(service=service)
-    driver.implicitly_wait(1)
-    yield driver
-    driver.quit()
+def driver(request):
+   browser = request.config.getoption("--browser", default="chrome").lower()
+   if browser == "chrome":
+      driver = webdriver.Chrome()
+   elif browser == "edge":
+      driver = webdriver.Edge()
+   elif browser == "firefox":
+      driver = webdriver.Firefox()
+   else:
+      raise ValueError("Unsupported browser {browser}")
+   
+   driver.maximize_window()
+   
+   driver.implicitly_wait(5)
+   
+   yield driver
+   
+   driver.quit()
